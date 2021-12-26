049692f0a2c1f5c7440f6657da4a3d8b85c8d0c7
Kelechi <kelechiuma@gmail.om>
2021-12-26 10:26:09 +0100

    Managing global state

---
+++ src/data/feedbackData.js
@@ -0,0 +1,30 @@
+const feedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima amet ipsa iusto repellat fugiat? Minus!',
+  },
+
+  {
+    id: 2,
+    rating: 9,
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima amet ipsa iusto repellat fugiat? Minus!',
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima amet ipsa iusto repellat fugiat? Minus!',
+  },
+  {
+    id: 4,
+    rating: 7,
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima amet ipsa iusto repellat fugiat? Minus!',
+  },
+  {
+    id: 5,
+    rating: 6,
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima amet ipsa iusto repellat fugiat? Minus!',
+  },
+];
+
+export default feedbackData;
